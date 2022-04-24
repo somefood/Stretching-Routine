893f696cd0724a155fcf7836bab8a94fc2d8f1f2
Seokju Hong <iamseokjuhong@gmail.com>
2022-04-24 21:04:51 +0900

    server init

---
+++ server/stretching/src/main/java/com/somefood/stretching/StretchingApplication.java
@@ -0,0 +1,13 @@
+package com.somefood.stretching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StretchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StretchingApplication.class, args);
+    }
+
+}

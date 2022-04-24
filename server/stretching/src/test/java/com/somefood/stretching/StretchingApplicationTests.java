ccabc015617693b8d0c2f1a538cbf58a1b80ee96
Seokju Hong <iamseokjuhong@gmail.com>
2022-04-24 21:04:51 +0900

    server init

---
+++ server/stretching/src/test/java/com/somefood/stretching/StretchingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somefood.stretching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StretchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
